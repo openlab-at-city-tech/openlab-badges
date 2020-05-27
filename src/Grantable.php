b9a10f1b95e97a2bf683ace9d3cc84cb148a3cef
Boone B Gorges <boonebgorges@gmail.com>
2020-05-27 16:38:00 -0500

    PHPCS for Grantable.

--- src/Grantable.php
+++ src/Grantable.php
@@ -1,7 +1,18 @@
 <?php
+/**
+ * Interface for Grantable objects.
+ */
 
 namespace OpenLab\Badges;
 
+/**
+ * Interface for Grantable objects.
+ */
 interface Grantable {
+	/**
+	 * Gets the ID of the grantable object.
+	 *
+	 * @return int
+	 */
 	public function get_id();
 }
